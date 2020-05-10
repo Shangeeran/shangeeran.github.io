1bab2b8cb2fd31c0020b12998ff7c54fa7aee4a4
unknown <shangeeran@gmail.com>
2020-05-10 02:43:43 +0000

    php file add for sent data to mail

--- getmail.php
+++ getmail.php
@@ -1,34 +1,20 @@
 <?php
-if (isset($_POST['name']) && isset($_POST['mail']) && isset($_POST['subject']) && isset($_POST['message'])) {
-    $name = $_POST['name'];
-    $mail = $_POST['mail'];
-    $subject = $_POST['subject'];
-    $message = $_POST['message'];
-    $to = 'shangeeran@gmail.com';
-    $body = '<html>
-                <body>
-                    <h2>Feedback</h2>
-                    <hr>
-                    <p>Name:<br>'.$name.'</p>
-                    <p>Email:<br>'.$mail.'</p>
-                    <p>Subject:<br>'.$subject.'</p>
-                    <p>Message:<br>'.$message.'</p>
-                </body>
-            </html>';
+	if(isset($_POST['submit'])){
+		$name=$_POST['name'];
+		$email=$_POST['email'];
+		$subject=$_POST['subject'];
+		$msg=$_POST['message'];
 
-// headers
-$headers  = "Form: ".$name."<".$mail."<".$subject."<".$message.">\r\n";
-$headers .= "REply-To: ".$mail."\r\n";
-$headers .= "Mine-Version: 1.0\r\n";
-$headers .= "Content-type: text/html; charset-utf-8";
+		$to='shangeeran@gmail.com'; // Receiver Email ID, Replace with your email ID
+		// $subject='Form Submission';
+		$message="Name :".$name."\n"."Subject :".$subject."\n"."Wrote the following :"."\n\n".$msg;
+		$headers="From: ".$email;
 
-//sent
-$sent = mail($to, $body, $headers);
-if ($sent) {
-    echo '<br>';
-    echo 'Thanks';
-} else {
-    echo 'error';
-}
-} 
+		if(mail($to, $message, $headers)){
+			echo "<h1>Sent Successfully! Thank you"." ".$name.", We will contact you shortly!</h1>";
+		}
+		else{
+			echo "Something went wrong!";
+		}
+	}
 ?>
